fb09ef3f9605307955d756584d14ceaacf9acf1d
Hiten Vishwakarma <hiten997764@gmail.com>
2021-09-11 13:52:50 +0530

    done

--- app.js
+++ app.js
@@ -4,8 +4,30 @@ var txtInput = document.querySelector("#txt-input");
 
 var output = document.querySelector("#output");
 
-function clickEventHandler() {
-  output.innerText = "translated: " + txtInput.value;
+//var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(input) {
+  return serverUrl + "?" + "text=" + input;
 }
 
-btnTranslate.addEventListener("click", clickEventHandler );
+function errorHandler(error) {
+  console.log("error occured ", error);
+  alert("Something wrong with server! Try again after sometime");
+}
+
+function clickEventHandler() {
+  var inputText = txtInput.value; // Taking input
+
+  // calling server for processing
+  fetch(getTranslationUrl(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      output.innerText = translatedText;// output
+    })
+    .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
